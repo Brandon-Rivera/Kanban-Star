aea98706b8cc0a71ec9f7782d5dd53685f81bd38
Brandon-Rivera <A01424338@tec.mx>
2023-03-26 13:46:22 -0600

    Login base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from "./Login";
+import { Workspace } from "./components/Workspace";
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/workspace" element={<Workspace />}></Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/components/Workspace.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Workspace.css"
+
+export const Workspace = () => {
+  return (
+    <div>Workspace</div>
+  )
+}
+
+export default Workspace
