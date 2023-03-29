b7f2cad86f2c48473bd2c0947e6922b380c088f3
Brandon-Rivera <A01424338@tec.mx>
2023-03-28 21:01:13 -0600

    Ya manda peticion

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,39 +1,40 @@
 import React, { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 export const Login = (props) => {
-    const form = useRef();
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        //const formData = new FormData(form.current);
-        console.log(email);
-        console.log(pass);
-        const values = {email: email, pass: pass}
+        const values = {
+            email: email,
+            pass: pass
+        };
+        console.log(values);
 
-        const response = await fetch(`https://university6y.kanbanize.com/index.php/api/kanbanize/login/`, { mode: 'no-cors'},
-            {method: 'POST', body: JSON.stringify(values)}
-        );
+        const response = await fetch(`http://localhost:3001/login`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values)
+            });
+
         const data = await response.json();
-
-        console.log('apikey: ', data.apikey);
-
-        if(data.apikey !== ''){
-            localStorage.setItem('apikey', data.apikey);
-            navigate("/workspace");
-        }
+        console.log(data);
     }
 
     return (
         <div className="auth-form-container">
             <h1>¡Bienvenido!</h1>
             <h2>Introduce tus credenciales de Kanbanize</h2>
-            <form className="login-form" ref={form} onSubmit={handleSubmit}>
+            <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">correo electrónico</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="tucorreo@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="tucorreo@gmail.com" id="email" name="email" />
                 <label htmlFor="password">contraseña</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="pass" name="pass" />
                 <button type="submit">Iniciar sesión</button>
