d4f333066a142239bf0f3d573dac43c8c404c94d
Brandon-Rivera <A01424338@tec.mx>
2023-04-03 16:51:46 -0600

    Arreglar index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './i18n'
-import {i18next} from './i18n'
-import { I18nextProvider} from 'react-i18next';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+import global_en from "./translations/en/global.json"
+import global_es from "./translations/es/global.json"
+
+
+i18next.init({
+  interpolation: {escapeValue: false},
+  lng: "es", //Lenguaje inicial
+  resources: {
+    es: {
+      global: global_es
+    },
+    en: {
+      global: global_en
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
