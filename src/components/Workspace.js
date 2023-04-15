c0751c0140fefe4f5d8c213da39f65beb5a8cfc8
Brandon-Rivera <A01424338@tec.mx>
2023-04-14 20:23:10 -0600

    Workspace dinamico

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -4,33 +4,35 @@ import './Workspace.css'
 
 export function Workspace() {
 
-    const [dataBoard, setDataBoard] = useState({ data: []});
-
-    const values = {
-        domain: "university6y",
-        userid: 7,
-        apikey: localStorage.getItem('apikey')
-    }
-
-    const getBoards = async () => {
-
-        const response = await fetch(`http://localhost:3001/dashboard/`, {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(values)
-        })
-        const data = await response.json()
-        console.log('data2', data)
-        setDataBoard(data)
-    }
+    //Variable para obtener los datos del workspace en un hook
+    const [dataBoard, setDataBoard] = useState({ data: [] });
 
     useEffect(() => {
+        
+        //Valores necesarios para la peticion get de workspace
+        const values = {
+            domain: localStorage.getItem('domain'),
+            userid: localStorage.getItem('userid'),
+            apikey: localStorage.getItem('apikey')
+        }
+        
+        //Funcion para realizar la peticion y almacenarlo en el hook dataBoard
+        const getBoards = async () => {
+
+            const response = await fetch(`http://localhost:3001/dashboard/`, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(values)
+            })
+            const data = await response.json()
+            setDataBoard(data)
+        }
+
+        //llamada a la funcion
         getBoards()
     }, [])
-
-    console.log('databoard', dataBoard)
 
     return (
         <div className="box">
@@ -39,16 +41,16 @@ export function Workspace() {
                     <div className='workspaceItem rounded'>
                         <h2 key={data.workspace_id}>{data.name}</h2>
                         <div className="row w-100">
-                        {
+                            {
                                 data.boards.map(boards => (
-                                <div className="col-md-4 mb-3" key={boards.board_id}>
-                                    <WorkCard title={boards.name} />
-                                </div>
-                            ))
-                        }
+                                    <div className="col-md-4 mb-3" key={boards.board_id}>
+                                        <WorkCard title={boards.name}/>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
-                ))   
+                ))
             }
         </div>
     )
