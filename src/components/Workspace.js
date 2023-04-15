9a0d0ab8626593fbd8da4d7ad26a7940a188458b
SnipperPlayer <galeana2002@hotmail.com>
2023-04-14 18:25:07 -0600

    Workspace v4.0 Completado

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,57 +2,6 @@ import React, { useEffect, useState } from 'react'
 import WorkCard from './WorkCard'
 import './Workspace.css'
 
-// const data = [
-//     {
-//         workspace_id: 2,
-//         type: "1",
-//         is_archived: "0",
-//         name: "Training",
-//         boards: [
-//             {
-//                 board_id: 4,
-//                 name: "LCRC"
-//             },
-//             {
-//                 board_id: 7,
-//                 name: "Alejandro Hidalgo Badillo"
-//             },
-//             {
-//                 board_id: 10,
-//                 name: "Emmanuel"
-//             },
-//             {
-//                 board_id: 19,
-//                 name: "Equipo 2"
-//             }
-//         ]
-//     },
-//     {
-//         workspace_id: 3,
-//         type: "1",
-//         is_archived: "0",
-//         name: "Training2",
-//         boards: [
-//             {
-//                 board_id: 5,
-//                 name: "LCRC2"
-//             },
-//             {
-//                 board_id: 8,
-//                 name: "Alejandro Hidalgo Badillo2"
-//             },
-//             {
-//                 board_id: 11,
-//                 name: "Emmanuel2"
-//             },
-//             {
-//                 board_id: 18,
-//                 name: "Equipo 3"
-//             }
-//         ]
-//     }
-// ]
-
 export function Workspace() {
 
     const [dataBoard, setDataBoard] = useState({ data: []});
@@ -73,60 +22,37 @@ export function Workspace() {
             body: JSON.stringify(values)
         })
         const data = await response.json()
-        console.log('data', data)
+        console.log('data2', data)
         setDataBoard(data)
     }
 
     useEffect(() => {
         getBoards()
-    }, [dataBoard])
+    }, [])
 
     console.log('databoard', dataBoard)
-    console.log('databoard.data[0].name', dataBoard.data[0].name)
 
     return (
         <div className="box">
             {
-                Array.isArray(dataBoard) && dataBoard.data.map(dataBoard => (
+                dataBoard.data.map(data => (
                     <div className='workspaceItem rounded'>
-                        <h2 key={dataBoard.data.workspace_id}>{dataBoard.data.name}</h2>
-                        {/* <div className="row w-100">
-                            {
-                                dataBoard.data[0].board.map(board => (
-                                    <div className="col-md-4 mb-3" key={board.board_id}>
-                                        <WorkCard title={board.name} />
-                                    </div>
-                                ))
-                            }
-                        </div> */}
+                        <h2 key={data.workspace_id}>{data.name}</h2>
+                        <div className="row w-100">
+                        {
+                                data.boards.map(boards => (
+                                <div className="col-md-4 mb-3" key={boards.board_id}>
+                                    <WorkCard title={boards.name} />
+                                </div>
+                            ))
+                        }
+                        </div>
                     </div>
-                ))
+                ))   
             }
         </div>
-
     )
 }
 
 export default Workspace
-
-/*
-    <div className="custom">
-        {
-        data.map(data => (
-            <h2 key={data.workspace_id}>
-                {data.name}
-            </h2>
-        ))
-        }
-        <div className="row w-100">
-            {
-                data[0].boards.map(board => (
-                    <div className = "col-md-4 mb-3" key={board.board_id}>
-                        <WorkCard title={board.name}/>
-                    </div> 
-                ))
-            }
-        </div>
-    </div>
-*/
 
