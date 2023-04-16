c7acef670d71678a6fd16c9e6d0c9be631eae0d1
Brandon-Rivera <A01424338@tec.mx>
2023-04-16 17:37:48 -0600

    Agregar input search

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import "./Board.css"
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export const Board = () => {
     return (
         <>
-            <ListGroup as="ul">
-                <ListGroup.Item className='title' as="li" active>
-                    Cras justo odio
+            <ListGroup>
+                <ListGroup.Item className='title' active>
+                    <h4>Tablero 1</h4>
+                    <InputGroup className="mb-3">
+                        <Button variant="dark" id="button-addon1">
+                            Button
+                        </Button>
+                        <Form.Control
+                            aria-label="Example text with button addon"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
                 </ListGroup.Item>
             </ListGroup>
         </>
