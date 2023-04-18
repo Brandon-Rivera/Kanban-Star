8b62f608e1f776df12987b15949ee8f7647407e4
Jonathan Maya <56286074+Yony55@users.noreply.github.com>
2023-04-18 13:25:44 -0600

    Logo de Kanban Star en Log In y fuente de Kanbanize (Mont Heavy).

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import ErrorModal from "./ErrorModal";
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
+import ksLogo from '../images/ksLogo.PNG'
 
 //import { changeLanguage } from "i18next";
 
@@ -65,7 +66,7 @@ export const Login = (props) => {
                 </center>
 
                 {/* Formulario de inicio de sesion */}
-                <h1 className="derecha">{t("login.hello")}</h1>
+                <h1 className="mont"><img className="kanbanStarLogo" src={ksLogo} alt="KanbanStar Logo"></img>kanban star</h1>
                 <h1>{t("login.welcome")}</h1>
                 <h2>{t("login.login-with-credentials")}</h2>
                 <form className="login-form" onSubmit={handleSubmit}>
