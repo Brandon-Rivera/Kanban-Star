f764da5cae3e7d851134c8522c33685e5772ff53
Brandon-Rivera <A01424338@tec.mx>
2023-04-26 18:07:49 -0600

    Funciones para menu

--- src/components/MenuTarjetas.js
+++ src/components/MenuTarjetas.js
@@ -8,25 +8,46 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function MenuTarjetas({ show, title, onHide }) {
+
+  const consultar = () => {
+    console.log("Consultar")
+  }
+
+  const mover = () => {
+    console.log("Mover")
+  }
+
+  const actualizar = () => {
+    console.log("Actualizar")
+  }
+
+  const comentarios = () => {
+    console.log("Comentarios")
+  }
+
+  const eliminar = () => {
+    console.log("Eliminar")
+  }
+
   return (
     <>
       <Modal show={show} onHide={onHide} size='lg' centered className='text-center'>
         <Modal.Header className='bg-success'>
-          <Modal.Title className='fw-bold'>{title}</Modal.Title><CloseButton onClick={onHide}/>
+          <Modal.Title className='fw-bold text-white'>{title}</Modal.Title><CloseButton onClick={onHide}/>
         </Modal.Header>
         <Modal.Body className='p-1'>
           <Container>
             <Row>
-              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100'>Consultar</Button></Col>
-              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100'>Mover</Button></Col>
+              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100' onClick={() => consultar()}>Consultar</Button></Col>
+              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100' onClick={() => mover()}>Mover</Button></Col>
             </Row>
             <Row>
-              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100'>Actualizar</Button></Col>
-              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100'>Comentarios</Button></Col>
+              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100' onClick={() => actualizar()}>Actualizar</Button></Col>
+              <Col className='p-1 m-1 d-flex justify-content-center'><Button className='fw-bold text-dark border border-3 border-dark d-block w-100' onClick={() => comentarios()}>Comentarios</Button></Col>
             </Row>
             <Row className='d-flex justify-content-center'>
               <Col className='p-0'></Col>
-              <Col><Button variant='danger' className='fw-bold text-dark border border-3 border-dark'>Eliminar</Button></Col>
+              <Col><Button variant='danger' className='fw-bold text-white border border-3 border-dark pl-5 pr-5' onClick={() => eliminar()}>Eliminar</Button></Col>
               <Col className='p-0'></Col>
             </Row>
           </Container>
