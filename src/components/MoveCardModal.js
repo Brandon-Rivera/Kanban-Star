33fc7f69b224249a9bcf594dad70ccf8f5b22272
Brandon-Rivera <A01424338@tec.mx>
2023-04-28 18:23:24 -0600

    Agregar nombre de carril

--- src/components/MoveCardModal.js
+++ src/components/MoveCardModal.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { Modal } from 'react-bootstrap';
+import CloseButton from 'react-bootstrap/CloseButton';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function MoveCardModal({show, onHide}) {
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+
+import { BsRecordCircleFill } from "react-icons/bs"
+
+function MoveCardModal({ show, onHide }) {
     return (
         <Modal show={show} onHide={onHide} size='lg' centered>
-            <Modal.Header>Seleccionar carril</Modal.Header>
+            <Modal.Header className='bg-success'>
+                <Modal.Title className='fw-bold text-white'>Selecciona carril</Modal.Title><CloseButton onClick={onHide} />
+            </Modal.Header>
             <Modal.Body>
-                ZS
+
+                <Form>
+                    {['checkbox'].map((type) => (
+                        <div key={`reverse-${type}`} className="mb-3">
+                            <Row>
+                                <Col className='d-flex justify-content-center m-0 p-0' centered>
+                                    <BsRecordCircleFill className='d-flex justify-content-center m-1' size={25}/>
+                                    <h3>hola</h3>
+                                </Col>
+                                <Col className='m-0 p-0'>
+                                    <Form.Check
+                                        reverse
+                                        name="group1"
+                                        type={type}
+                                        id={`reverse-${type}-1`}
+                                    />
+                                </Col>
+                            </Row>
+                        </div>
+                    ))}
+                </Form>
+
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={onHide}>Aceptar</Button>
--- src/components/MoveModal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MoveModal() {
-  return (
-    <div>MoveModal</div>
-  )
-}
-
-export default MoveModal
